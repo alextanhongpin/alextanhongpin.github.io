0a035a5769ac3ec15ba9f36b6472c5bfdfff2091
Alex Tan Hong Pin <alextan220990@gmail.com>
2014-12-05 17:43:21 +0800

    latest-revision

---
+++ js/templates.js
@@ -0,0 +1,71 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["books"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<div class="col-8" style="font-size: 1.5em;">"I cannot remember the books I\'ve read any more than the meals I have eaten; even so, they have made me." - <b>Ralph Waldo Emerson</b></div><br>\n\n<ul class="col-9">\n\t<li><b>Emotional Intelligence</b> - Daniel Goleman</li>\n\t<li><b>The Gift of Fear</b> - Gavin de Becker</li>\n\t<li><b>Influence: The Psychology of Persuasion</b> - Robert B. Cialdini</li>\n\t<li><b>The 48 Laws of Powers</b> - Robert Greene</li>\n\t<li><b>The Art of Seduction</b> - Robert Greene</li>\n\t<li><b>Mastery</b> - Robert Greene</li>\n\t<li><b>The Tipping Point: How Little Things can Make a Big Difference</b> - Malcolm T. Gladwell</li>\n\t<li><b>Blink: The Power of Thinking Without Thinking</b> - Malcolm T. Gladwell</li>\n\t<li><b>Outliers: The Story of Success</b> - Malcolm T. Gladwell</li>\n\t<li><b>What the Dog Saw: And other Adventures</b> - Malcolm T. Gladwell</li>\n\t<li><b>David and Goliath: Underdogs, Misfits, and the Art of Battling Giants</b> - Malcolm T. Gladwell</li>\n\t<li><b>Lateral Thinking</b> - Edward de Bono</li>\n\t<li><b>Simplicity</b> - Edward de Bono</li>\n\t<li><b>Six Thinking Hats</b> - Edward de Bono</li>\n\t<li><b>Po: Beyond Yes and No</b> - Edward de Bono</li>\n\t<li><b>Emotional Blackmail: When the People in Your Life Use Fear, Obligation, and Guilt to Manipulate You</b> - Susan Forward</li>\n\t<li><b>Games People Play: The Psychology of Human Relationships</b> - Eric Berne</li>\n\t<li><b>50 Psychology Classics: Who We Are, How We Think, What We Do: Insight and Inspiration from 50 Key Books</b> - Tom Butler Bowdown</li>\n\t<li><b>The Psychology of Self-Esteem</b> - Nathaniel Branden</li>\n\t<li><b>Creativity: Flow and the Psychology of Discovery and Inventions</b> - Mihaly Csikzentmihalyi</li>\n\t<li><b>My Voice Will Go With You</b> - Milton Erikson</li>\n\t<li><b>How Technology is Changing our Minds for the Better.</b> - Clive Thompson</li>\n</ul>\n\n';
+
+}
+return __p
+};
+
+this["JST"]["drawing"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<h1>T-Shirts Design</h1>\n<br>\n\t<div class="col-7"><img src="/img/tshirt/eau-de-vie-the-unthinkable-depth.jpg" alt="img">  <span>The Unthinkable Depth.</span></div>\n\t<div class="col-7"><img src="/img/tshirt/what-the-gummibear-saw.jpg" alt="img">    <span>What the Gummibear Saw.</span></div>\n\t<div class="col-7"><img src="/img/tshirt/toxic.jpg" alt="img">    <span>A Gentle Toxic.</span></div>\n\t<div class="col-7"><img src="/img/tshirt/great-design-submission.jpg" alt="img">    <span>Rotes Gestalt.</span></div>\n\t<div class="col-7"><img src="/img/tshirt/investion-gone-wrong.jpg" alt="img">    <span>Investment Gone Wrong.</span></div>\n\t<div class="col-7"><img src="/img/tshirt/sub-template-tee-collab-(2).jpg" alt="img">  <span>Collaboration: Zen</span></div>\n<br>\n<p>The digital blacksmith of this era presents to you...</p>\n<h1>Swords</h1>\n<br>\n\n<div class="col-7"><img src="/img/swords/s01_prophet\'s-sword.jpg" alt="img">\n<span>Prophet"s Sword: A legendary weapon so powerful that its existence is denied by gods.</span></div>\n<div class="col-7"><img src="/img/swords/s02_sunslayer.jpg" alt="img">\n<span>Sunslayer: It is said that this sword fueled the sun for centuries.</span></div>\n\n<div class="col-7"><img src="/img/swords/s03_samurai\'s-third-sword.jpg" alt="img">\n<span>Samurai"s Blade: A sword forged by the legendary samurai Mushashi.</span></div>\n\n<div class="col-7"><img src="/img/swords/s04_vampire.jpg" alt="img">\n<span>Vampire: This sword drains the blood of its victim, leaving them a bloodless corpse.</span></div>\n\n\n<div class="col-7"><img src="/img/swords/unknown.jpg" alt="img">\n<span>Unknown Sword. Check out for new release.</span></div>\n\n\n<p>The next work will be...</p>\n<h1>Original Manga Characters</h1>\n<div class="col-7"><img src="/img/swords/unknown.jpg" alt="img">\n<span>Coming soon.</span></div>\n\n';
+
+}
+return __p
+};
+
+this["JST"]["guitar"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '\n    <iframe class="col-8" height="166" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/150323475&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>\n      <iframe class="col-8" height="166" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/150634086&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>\n      <iframe class="col-8" height="166" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/153449882&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>\n      <iframe class="col-8" height="166" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/157502847&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>\n\n\t\n';
+
+}
+return __p
+};
+
+this["JST"]["navbar"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<header class="col-2"> <h3><a href="/"> alextanhongpin </a></h3> </header>\n<li class="col-2"> <a href="books"> <i class="fa fa-book"></i>&nbsp;&nbsp;&nbsp;&nbsp; Books </a> </li>\n<li class="col-2"> <a href="codes"> <i class="fa fa-code"></i>&nbsp;&nbsp;&nbsp;&nbsp; Codes </a> </li>\n<li class="col-2"> <a href="drawing"> <i class="fa fa-pencil-square-o"></i>&nbsp;&nbsp;&nbsp;&nbsp; Drawing </a> </li>\n<li class="col-2"> <a href="guitar"> <i class="fa fa-music"></i>&nbsp;&nbsp;&nbsp;&nbsp; Guitar </a> </li>\n<li class="col-2"> <a href="photography"> <i class="fa fa-camera-retro"></i>&nbsp;&nbsp;&nbsp;&nbsp; Photography </a> </li>\n<li class="col-2"> <a href="resume"><i class="fa fa-envelope"></i>&nbsp;&nbsp;&nbsp;&nbsp; Resume </a> </li>\n\n';
+
+}
+return __p
+};
+
+this["JST"]["profile"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<p class="intro">Hello <strike>World!</strike> Alex!</p><p>That\'s the first program I wrote.</p>';
+
+}
+return __p
+};
+
+this["JST"]["programming"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '<h1>*to be updated*</h1>\n';
+
+}
+return __p
+};
+
+this["JST"]["resume"] = function(obj) {
+obj || (obj = {});
+var __t, __p = '', __e = _.escape;
+with (obj) {
+__p += '      <table>\n        <!--row 1-->\n        <tr>\n            <td class="col-2"><b>Personal Data</b></td>\n            <td class="col-5"></td>\n            <td class="col-2"><img id=\'profile\' src="/img/profilepic.jpg" alt="img"></td>\n        </tr>\n        <!--row 2-->\n        <tr>\n            <td class="col-2">First name:</td>\n            <td class="col-5">Alex Hong Pin</td>\n            <td class="col-2"></td>\n        </tr>\n        <!--row 3-->\n        <tr>\n            <td class="col-2">Last name:</td>\n            <td class="col-5">Tan</td>\n            <td class="col-2"></td>\n        </tr>\n        <!--row 4-->\n        <tr>\n            <td class="col-2">Date of Birth:</td>\n            <td class="col-5">22 September 1990</td>\n            <td class="col-2"></td>\n        </tr>\n        <!--row 5-->\n        <tr>\n            <td class="col-2">Place of Birth:</td>\n            <td class="col-5">Georgetown, Penang</td>\n            <td class="col-2"></td>\n        </tr> \n        <!--row 7-->\n        <tr>\n            <td class="col-2">Gender:</td>\n            <td class="col-5">male</td>\n            <td class="col-2"></td>\n        </tr>\n         <!--row 8-->\n        <tr>\n            <td class="col-2">Status:</td>\n            <td class="col-5">single</td>\n            <td class="col-2"></td>\n        </tr> \n          <!--row 9-->\n        <tr>\n            <td class="col-2">Nationality:</td>\n            <td class="col-5">Malaysian</td>\n            <td class="col-2"></td>\n        </tr>\n          <tr><td><br></td><td></td><td></td></tr>\n        <tr>\n            <td class="col-2"><b>Education</b></td>\n            <td class="col-5"></td>\n            <td class="col-2"></td>\n        </tr>\n            <!--row 10-->\n        <tr>\n            <td class="col-2">01/1997 - 12/2002:</td>\n            <td class="col-5">Sekolah Kebangsaan Ibrahim, Sungai Petani, Kedah.</td>\n            <td class="col-2"></td>\n        </tr>\n        <tr>\n            <td class="col-2"></td>\n            <td class="col-5">Ujian Pencapaian Sekolah Rendah (UPSR).</td>\n            <td class="col-2">Results: 5xA</td>\n        </tr>\n          <tr><td><br></td><td></td><td></td></tr>\n          <!--row 11-->\n        <tr>\n            <td class="col-2">01/2003 - 12/2005:</td>\n            <td class="col-5">Sekolah Menengah Kebangsaan Ibrahim, Sungai Petani, Kedah.</td>\n            <td class="col-2"></td>\n        </tr>\n        <tr>\n            <td class="col-2"></td>\n            <td class="col-5">Penilaian Menengah Rendah (PMR).</td>\n            <td class="col-2">Results: 6xA 1xB</td>\n        </tr>\n          <tr><td><br></td><td></td><td></td></tr>\n          <!--row 12-->\n        <tr>\n            <td class="col-2"> 01/2006 - 12/2007:</td>\n            <td class="col-5">Sekolah Menengah Kebangsaan Ibrahim, Sungai Petani, Kedah.</td>\n            <td class="col-2"></td>\n        </tr>\n        <tr>\n            <td class="col-2"></td>\n            <td class="col-5">Sijil Pelajaran Malaysia (SPM).</td>\n            <td class="col-2">Results: 12xA1</td>\n        </tr>\n          <tr><td><br></td><td></td><td></td></tr>\n           <!--row 13-->\n        <tr>\n            <td class="col-2">07/2008 - 05/2010:</td>\n            <td class="col-5">International Education Centre (INTEC), Universiti Teknologi Mara (UiTM), Shah Alam</td>\n            <td class="col-2"></td>\n        </tr>\n        <tr>\n            <td class="col-2"></td>\n            <td class="col-5">Edexcel A-Level</td>\n            <td class="col-2">AS-German 1xA</td>\n        </tr>\n         <tr>\n            <td class="col-2"></td>\n            <td class="col-5"></td>\n            <td class="col-2">Chemistry 1xA*</td>\n        </tr>\n        <tr>\n            <td class="col-2"></td>\n            <td class="col-5"></td>\n            <td class="col-2">Mathematics 1xA*</td>\n        </tr>\n        <tr>\n            <td class="col-2"></td>\n            <td class="col-5"></td>\n            <td class="col-2">Physics 1xA</td>\n        </tr>\n          <tr><td><br></td><td></td><td></td></tr>\n          <!--row 14-->\n        <tr>\n            <td class="col-2"> 07/2010 - 09/2010:</td>\n            <td class="col-5">International Education Centre (INTEC).</td>\n            <td class="col-2"></td>\n        </tr>\n        <tr>\n            <td class="col-2"></td>\n            <td class="col-5">DSH (Deutsche Sprachprüfung für den Hochschulzugang)</td>\n            <td class="col-2">Results: DSH 3 (1.8)</td>\n        </tr>\n          <tr><td><br></td><td></td><td></td></tr>\n          <!--row 15-->\n        <tr>\n            <td class="col-2"> 03/2011 - current day:</td>\n            <td class="col-5">Bachelor in Chemical Engineering</td>\n            <td class="col-2"></td>\n        </tr>\n        <tr>\n            <td class="col-2"></td>\n            <td class="col-5">The Mannheim University of Applied Sciences, Mannheim, Germany</td>\n            <td class="col-2"></td>\n        </tr>\n          <tr>\n            <td class="col-2"></td>\n            <td class="col-5">Foundation Course (Grundstudium)</td>\n            <td class="col-2">Results: 1.2 (Highest: 1.0)</td>\n        </tr>\n          <tr>\n            <td class="col-2"></td>\n            <td class="col-5">Main Course (Hauptstudium)</td>\n            <td class="col-2">Results: t.b.a (Highest: 1.0)</td>\n        </tr>\n          \n      </table>\n\n      ';
+
+}
+return __p
+};
